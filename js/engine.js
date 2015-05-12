b68d51bda81dfa600039987f69ca24e4e94c705a
gincwang <ProRage@Gins-MacBook-Pro.local>
2015-05-12 09:47:12 -0700

    Add encoding for space (%20) in gem image url

--- js/engine.js
+++ js/engine.js
@@ -264,9 +264,9 @@ var Engine = (function(global) {
         'images/char-horn-girl.png',
         'images/char-pink-girl.png',
         'images/char-princess-girl.png',
-        'images/Gem Blue.png',
-        'images/Gem Green.png',
-        'images/Gem Orange.png',
+        'images/Gem%20Blue.png',
+        'images/Gem%20Green.png',
+        'images/Gem%20Orange.png',
         'images/Selector.png',
         'images/Heart.png'
     ]);
